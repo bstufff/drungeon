24ae6d99125ac396fec307136be3f9c69de6c4a2
bstuff <arthur.bohn@lcieducation.net>
2024-09-20 14:51:58 -0400

    système ennemi
    ajout du spawn d'ennemi et du mouvement d'ennemi.

---
+++ Assets/Arthur/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int hp = 100;
+    public int maxHP = 100;
+    public int speed = 10;
+    public Path path;
+    public LevelManager levelManager;
+    public Enemy(int hp, int maxHP, int speed, Path path, LevelManager levelManager)
+    {
+        this.hp = hp;
+        this.maxHP = maxHP;
+        this.speed = speed;
+        this.path = path;
+        this.levelManager = levelManager;
+    }
+
+}
---
+++ Assets/Arthur/Scripts/Path.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class Path
+{
+    public List<Vector3> path;
+}
